8d07adcff90ae8e50f474870374462b04bdc0402
gabrielamuller <mullergabrielaa@gmail.com>
2022-09-06 14:19:10 +0300

    2.12 Add a search field for countries and show country name, capital, area and flag

--- part-2/countries/src/App.js
+++ part-2/countries/src/App.js
@@ -1,7 +1,57 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = ({ onChange, value }) => {
+  return (
+    <>
+      <div>
+        Find countries <input onChange={onChange} value={value} type='search' />
+      </div>
+    </>
+  );
+};
+
+const Countries = ({filtered}) => {
+  return (
+    filtered.map((country) =>
+    <div key={country.name.common}>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <img src={country.flags.png} alt='' />
+    </div>
+    )
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setNewSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+ const handleSearch = (event) => {
+  console.log(event.target.value)
+  setNewSearch(event.target.value);
+}
+
+const filtered = countries.filter((country) =>
+  country.name.common.toLowerCase().includes(search.toLowerCase())
+);
+console.log(filtered);
+
+  return (
+    <div>
+      <Filter onChange={handleSearch} value={search}/>
+      <Countries filtered={filtered}/>
+    </div>
+  )
+}
 
 export default App
