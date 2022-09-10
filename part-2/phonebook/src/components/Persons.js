9478b5be3bbebbce28a9cbaae9f614967c326cee
gabrielamuller <mullergabrielaa@gmail.com>
2022-09-10 16:03:31 +0300

    Add some space between the text and delete button

--- part-2/phonebook/src/components/Persons.js
+++ part-2/phonebook/src/components/Persons.js
@@ -4,7 +4,7 @@ const Persons = ({filtered, request}) => {
 	return (
 	  filtered.map((person) =>
 		<p key={person.id}>
-			{person.name} {person.number}
+			{person.name} {person.number} {''}
 			<Button text='delete' onClick={() =>
 				window.confirm(`Delete ${person.name}`) && request(person.id)
 			} />
